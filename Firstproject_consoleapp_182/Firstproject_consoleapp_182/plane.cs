0e977b4136c1e6b98e95178feb999f394f684801
Atman Shastri <atmanshastri@gmail.com>
2023-03-14 14:50:09 +0530

    14th March Final AWT Commit

---
+++ Firstproject_consoleapp_182/Firstproject_consoleapp_182/plane.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firstproject_consoleapp_182
+{
+     abstract class plane
+    {
+        public abstract void ratesofplane();
+    }
+}
