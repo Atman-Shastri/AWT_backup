40d04d9d60bc1cfb875c10b53c7c456e344d5da9
Atman-Shastri <atmanshastri@gmail.com>
2023-03-20 00:29:07 +0530

    20th March 2023 Home Second Commit

---
+++ Firstproject_consoleapp_182/Firstproject_consoleapp_182/mainClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Firstproject_consoleapp_182
+
+{
+    class mainClass
+    {
+        static void Main(string[] args)
+        {
+            international i = new international();
+            domestic d = new domestic();
+            Console.WriteLine("Atman Shastri, 182");
+            Console.WriteLine("Enter number of seats: ");
+            int internationalSeats = Convert.ToInt32(Console.ReadLine());
+            i.getRate();
+            d.getRate();
+            i.calculate(internationalSeats);
+            d.calculate(internationalSeats);
+        }
+    }
+}
