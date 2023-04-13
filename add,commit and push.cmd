43629b25249fa047728e18b113e486c97eb76a61
Atman-Shastri <atmanshastri@gmail.com>
2023-04-13 19:02:35 +0530

    13-04-2023_19.02.35.50

--- add,commit and push.cmd
+++ add,commit and push.cmd
@@ -1,5 +1,5 @@
-git config --user.name "Atman-Shastri"
-git config --user.email "atmanshastri@gmail.com"
+git config user.name "Atman-Shastri"
+git config user.email "atmanshastri@gmail.com"
 git add .
-git commit -m %1
+git commit -m %date%_%time%
 git push
