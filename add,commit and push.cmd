cc2d5604219af3f83b0c604ce604d6e9d1593ce9
Atman-Shastri <atmanshastri@gmail.com>
2023-04-14 00:15:30 +0530

    14-04-2023 12.15 AM

--- add,commit and push.cmd
+++ add,commit and push.cmd
@@ -1,5 +1,7 @@
+for /F "tokens=1" %%i in ('time /t') do set time1=%%i
+for /F "tokens=2" %%i in ('time /t') do set ampm=%%i
 git config user.name "Atman-Shastri"
 git config user.email "atmanshastri@gmail.com"
 git add .
-git commit -m %date%_%time%
+git commit -m "%DATE% %time1% %ampm%"
 git push
