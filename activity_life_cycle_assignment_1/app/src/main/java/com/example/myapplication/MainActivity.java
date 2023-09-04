c5b7b3ddadb1d8baf5dfa672de0d187c35fe1ee9
Atman-Shastri <atmanshastri@gmail.com>
2023-09-04 16:50:34 +0530

    04-09-2023 16:50

--- activity_life_cycle_assignment_1/app/src/main/java/com/example/myapplication/MainActivity.java
+++ activity_life_cycle_assignment_1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -15,10 +15,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+    setContentView(R.layout.activity_start);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        setContentView(R.layout.activity_resume);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setContentView(R.layout.activity_pause);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        setContentView(R.layout.activity_stop);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        setContentView(R.layout.activity_destroy);
     }
 }
